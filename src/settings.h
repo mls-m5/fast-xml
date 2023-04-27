818261cd0279f68cd6c595eeb54d86f5e56f8d32
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-04-27 11:27:28 +0200

    Fix problem when input and output file is the same

--- src/settings.h
+++ src/settings.h
@@ -8,52 +8,25 @@
 
 class Settings {
 public:
-    Settings(int argc, char **argv)
-        : input_str{&std::cin}
-        , output_str{&std::cout} {
+    Settings(int argc, char **argv) {
         auto args = std::vector<std::string>{argv + 1, argv + argc};
 
         for (std::size_t i = 0; i < args.size(); ++i) {
             const auto &arg = args.at(i);
 
             if (arg == "--input" || arg == "-i") {
-                if (i + 1 < args.size()) {
-                    in_path = args.at(++i);
-                    input_file = std::ifstream{in_path};
-                    if (!input_file.is_open()) {
-                        std::cerr
-                            << "Error: Could not open input file: " << in_path
-                            << std::endl;
-                        std::exit(1);
-                    }
-                    input_str = &input_file;
-                    check_if_same();
-                }
-                else {
-                    std::cerr << "Error: No input file specified after: " << arg
+                in_path = args.at(++i);
+                input_file = std::ifstream{in_path};
+                if (!input_file.is_open()) {
+                    std::cerr << "Error: Could not open input file: " << in_path
                               << std::endl;
-                    print_help_and_exit();
+                    std::exit(1);
                 }
+                input_str = &input_file;
+                check_if_same();
             }
             else if (arg == "--output" || arg == "-o") {
-                if (i + 1 < args.size()) {
-                    out_path = args.at(++i);
-                    output_file = std::ofstream{out_path};
-                    if (!output_file.is_open()) {
-                        std::cerr
-                            << "Error: Could not open output file: " << out_path
-                            << std::endl;
-                        std::exit(1);
-                    }
-                    output_str = &output_file;
-                    check_if_same();
-                }
-                else {
-                    std::cerr
-                        << "Error: No output file specified after: " << arg
-                        << std::endl;
-                    print_help_and_exit();
-                }
+                out_path = args.at(++i);
             }
             else if (arg == "--help" || arg == "-h") {
                 print_help_and_exit();
@@ -62,6 +35,19 @@ public:
                 std::cerr << "Error: Unknown argument: " << arg << std::endl;
                 print_help_and_exit();
             }
+        }
+
+        // Must be last to make sure that nothing is written before the content
+        // is possibly copied
+        if (!out_path.empty()) {
+            check_if_same();
+            output_file = std::ofstream{out_path};
+            if (!output_file.is_open()) {
+                std::cerr << "Error: Could not open output file: " << out_path
+                          << std::endl;
+                std::exit(1);
+            }
+            output_str = &output_file;
         }
     }
 
@@ -77,7 +63,7 @@ public:
         if (input_str == &tmp) {
             return;
         }
-        if (in_path == out_path) {
+        if (std::filesystem::equivalent(in_path, out_path)) {
             tmp << input_str->rdbuf();
             input_str = &tmp;
         }
@@ -103,6 +89,6 @@ Options:
 
     std::ifstream input_file;
     std::ofstream output_file;
-    std::istream *input_str;
-    std::ostream *output_str;
+    std::istream *input_str = &std::cin;
+    std::ostream *output_str = &std::cout;
 };
